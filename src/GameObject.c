4fe66ae5f1eaebbc750ce492c1080880ee4b26cd
Hannu Viitala <hannuj.viitala@gmail.com>
2024-12-07 21:38:54 +0200

    one datapacket moves vertically

--- src/GameObject.c
+++ src/GameObject.c
@@ -1,3 +1,6 @@
+#include "lib/zxn/zxnext_layer2.h"
+#include "lib/zxn/zxnext_sprite.h"
+
 #include "GameObject.h"
 
 void GobUpdate(GameObject* gob)
@@ -6,11 +9,15 @@ void GobUpdate(GameObject* gob)
     // if(gob->x <  255 - gob->sx)
        gob->x += gob->sx;
     uint8_t orgY = gob->y;
-    // if(gob->y <  255 - gob->sy)
+    if(gob->y <  SCREEN_Y - gob->sy)
        gob->y += gob->sy;   
+    else
+       gob->y = 0;
 }
 
 void GobDraw(GameObject* gob)
 {
-
+    // Update sprite position.
+    set_sprite_slot(0);
+    set_sprite_attributes_ext(gob->spriteIndex, gob->x, gob->y, 0, 0, !gob->isHidden);
 }
--- src/GameObject.h
+++ src/GameObject.h
@@ -1,11 +1,18 @@
 #include <stdint.h>
 
+#define SCREEN_X 256
+#define SCREEN_Y 192
+
 typedef struct GameObject_ 
 {
     uint8_t x; // X coordinate in pixels
     uint8_t y; // Y coordinate in pixels
     int8_t sx; // Horizontal speed in pixels
     int8_t sy; // Vertical speed in pixels
+
+    uint8_t spriteIndex;  // Sprite index.
+    bool isHidden;  // True is the game objec is not visible.
+
 } GameObject;
 
 void GobUpdate(GameObject* gob);
